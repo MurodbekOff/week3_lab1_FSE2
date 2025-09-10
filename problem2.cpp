e58c1ae3f2cb05962eb16627615909f440c00285
MurodbekOff <muxtorovmurodbek007@gmail.com>
2025-09-10 12:00:08 +0500

    third commmit

---
+++ problem2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Muxtorov Murodbek"<<endl<<250292<<endl<<"Tame Impala - Let it happen"<<endl<<"Marvel - Infinity war";
+    return 0;
+}
