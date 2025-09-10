38f789acf13bc675667ac84c1157ef16ce2d26eb
MurodbekOff <muxtorovmurodbek007@gmail.com>
2025-09-10 12:41:31 +0500

    third commmit

---
+++ problem3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"\"I'm standing\" on the edge of some crazy cliff. "<<endl<<"What I have to do, I have to catch everybody\\ "<<endl;
+    cout<<" if they start to go over the cliff–I mean \\"<<endl<<" \\if they're running and they don't look"<<endl<<"where they're going I have to come out"<<endl<<"from somewhere and       catch them.";
+    return 0;
+}
---
+++ problem4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 189;
+    char b = 'B';
+    float thirds = a/3;
+
+    return 0;
+}
