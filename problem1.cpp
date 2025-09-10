d815b174603ba33fa5dc1fa1d631d9664b6a649d
MurodbekOff <muxtorovmurodbek007@gmail.com>
2025-09-10 11:53:27 +0500

    First commit

--- problem1.cpp
+++ problem1.cpp
@@ -4,5 +4,10 @@ int main() {
     int intNumber = 17;
     float floatNumber = 3.14;
     double doubleNumber = 45.1234;
+    bool boolean = true;
+    char charName = 'A';
+    cout<<intNumber<<", size = "<<sizeof(intNumber)<<endl<<floatNumber<<", size = "<<sizeof(floatNumber)<<endl<<doubleNumber<<", size = "<<sizeof(doubleNumber)<<endl;
+    cout<<boolean<<", size = "<<sizeof(boolean)<<endl;
+    cout<<charName<<", size = "<<sizeof(charName)<<endl;
     return 0;
 }
